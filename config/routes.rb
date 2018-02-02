0de03b313e3fa7f13497805ce80836672d317f53
Daniel <shiriux1@gmail.com>
2018-02-02 12:35:15 -0500

    fixed api issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'checkout#make_order'
-  post '/add', to: 'checkout#make_order'
-  get '/total', to: 'checkout#total_order'
-  get '/remove', to: 'checkout#remove'
+
+
+
+    namespace :api, defaults: { format: :json } do
+  		namespace :v1 do
+    		 post '/add', to: 'checkout#make_order'
+  			get '/total', to: 'checkout#total_order'
+  			get '/remove', to: 'checkout#remove'
+  		end
+	end
 end
